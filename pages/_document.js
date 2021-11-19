511f07f01963ef4261c4d64dda0292f6b19162cc
Juan <pront23@gmail.com>
2021-11-20 00:36:35 +0900

    added preloaded fonts

---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en" dir="ltr">
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/Poppins-Bold.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/Poppins-Regular.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/Poppins-SemiBold.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body className="bg-body text-white font-poppins pb-12">
+          <Main></Main>
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
